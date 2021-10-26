dff12bcfa337bb6b950df314cd1568621c99f37e
unknown <usmanarain7755@gmail.com>
2021-10-26 12:52:27 +0500

    my react assingment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import CardGallery from "./Cardgallery";
+
+function App() {
+  return (
+    <>
+
+      <CardGallery />
+    </>
+  );
+}
+
+export default App;
